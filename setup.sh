10409b43acd66822eb0973906417ffc6c7a487d7
Ihor16 <igor@LAPTOP-4L9KL5H8.localdomain>
2022-03-28 16:33:48 -0700

    updated setup documentation

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ mkdir -p ~/bin
 # Makes rn script executable
 chmod +x rn
 
-# Copies the rn script to you ~/bin directory
+# Copies the rn script to your ~/bin directory
 cp ./rn ~/bin/
 
 # Updates your PATH variable in .bashrc file
 echo "PATH=\$PATH:\$HOME/bin" >> ~/.bashrc
 
-# Refreshes your .bashrc to make the rn command available
+# Refreshes the shell session to make the rn command available
 . ~/.bashrc
 
