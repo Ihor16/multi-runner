8085b985c5edcf7f562f63663c321b482fbe303c
Ihor16 <igor@LAPTOP-4L9KL5H8.localdomain>
2022-03-28 22:21:48 -0700

    added symbolic link to setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# Creates ~/bin directory if you don't have it yet
+# Stores path to the repo's directory
+DIR=$(pwd)
+
+# Creates ~/bin directory if it doesn't exist
 mkdir -p ~/bin
 
 # Makes rn script executable
 chmod +x rn
 
-# Copies the rn script to your ~/bin directory
-cp ./rn ~/bin/
+# Creates a symbolic link to rn script in the ~/bin directory
+# Now any newer version of the rn will be automatically picked up in the ~/bin even if the script is renamed in the ~/bin
+cd ~/bin
+ln -s $DIR/rn .
 
-# Updates your PATH variable in .bashrc file
+# Goes back to the repo's directory
+cd $DIR
+
+# Updates PATH variable in .bashrc file
 echo "PATH=\$PATH:\$HOME/bin" >> ~/.bashrc
 
 # Refreshes the shell session to make the rn command available
